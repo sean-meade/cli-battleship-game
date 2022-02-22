f3d9c5d5d6a6cc71bce30357cd41c2c2fb7c36e7
Sean <seanpatmeade@gmail.com>
2022-02-22 21:05:00 +0000

    Add name input from userand add it to the players Board on creation. Add input for starting point and direction of ship placement. Create a function place_ship that take in the board, starting point, direction, and size. This function uses a match case to check direction of ship and then place it on the given board (does not yet check if it fits on the board

--- run.py
+++ run.py
@@ -4,8 +4,19 @@ class PlayerBoard:
     A class that creates a players side of the board and holds it's ships, hits and misses
     """
     
-    def __init__(self, player):
-        self.board = [[player] * 10] * 10
+    def __init__(self, player, name = "comp"):
+        self.board = [["w"] * 10,
+                      ["g"] * 10,
+                      ["h"] * 10,
+                      ["j"] * 10,
+                      ["k"] * 10,
+                      ["l"] * 10,
+                      ["a"] * 10,
+                      ["s"] * 10,
+                      ["d"] * 10,
+                      ["r"] * 10,]
+        # [[player] * 10] * 10
+        self.name = name
 
     def print_board(self):
         """ print the players board to the screen """
@@ -24,10 +35,55 @@ class PlayerBoard:
                 print(letters[line] + "  " + "|  " + '  '.join(self.board[line]) + "  |")
                 print('   |                                |')
 
-player_one = PlayerBoard("~")
 
-player_one.print_board()
+def place_ship(players_board, starting_point, direction, size):
+    """
+    Function takes a Board, a starting point on that Board, a direction and size of a ship and places it on the Board
+    """
+    SHIP = "#"
 
-computer = PlayerBoard("O")
+    x_and_y = list(starting_point)
 
-computer.print_board()
+    x_direction = int(x_and_y[0])
+    y_direction = 9 - (ord(x_and_y[1]) - 97)
+    # print("x", x_direction, "y", y_direction, "x and y", x_and_y)
+
+    direction_upper = direction.upper()
+
+    match direction_upper:
+        case "UP":
+            for y_up in range(y_direction - (size - 1), y_direction + 1):
+                players_board.board[y_up][x_direction] = SHIP
+
+        case "DOWN":
+            for y_down in range(y_direction, y_direction + size):
+                players_board.board[y_down][x_direction] = SHIP
+
+        case "RIGHT":
+            for x_right in range(x_direction, x_direction + size):
+                players_board.board[y_direction][x_right] = SHIP
+
+        case "LEFT":
+            for x_left in range(x_direction  - (size - 1), x_direction + 1):
+                players_board.board[y_direction][x_left] = SHIP
+
+    players_board.print_board()
+
+
+def main():
+    """
+    Main function ran on execution of file
+    """
+    name = input("Please give your username:\n")
+    player_one = PlayerBoard("~", name)
+    starting_point = input("Choose starting point of ship (e.g. 6f, 7y, 3i):\n")
+    direction = input("Choose direction of the ship (e.g. UP, DOWN, LEFT, or RIGHT):\n")
+    place_ship(player_one, starting_point, direction, 2)
+
+    
+
+    # computer = PlayerBoard("O")
+
+    # computer.print_board()
+
+main()
