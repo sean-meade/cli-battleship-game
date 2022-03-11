8e64516c8beeb624c8b2035de5eb9c72748281b2
Sean <seanpatmeade@gmail.com>
2022-03-11 01:36:39 +0000

    Reformat leaderboard.py to PEP8 with black.vercel.app

--- leaderboard.py
+++ leaderboard.py
@@ -6,16 +6,16 @@ from termcolor import colored
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
+    "https://www.googleapis.com/auth/drive",
+]
 
 # Create credentials for the scope and create a 'client'
-CREDS = Credentials.from_service_account_file('creds.json')
+CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 
 # Open the google sheet
-SHEET = GSPREAD_CLIENT.open('leader-board')
+SHEET = GSPREAD_CLIENT.open("leader-board")
 
 
 def add_name_and_time_to_leaderboard(name, time, secs):
@@ -27,7 +27,7 @@ def add_name_and_time_to_leaderboard(name, time, secs):
     secs = time taken to win the game in seconds
     """
     # Add user and time to googlesheet
-    leader_board = SHEET.worksheet('scores')
+    leader_board = SHEET.worksheet("scores")
     leader_board.append_row([name, time, secs])
 
 
@@ -36,9 +36,9 @@ def print_top_players():
     A function that prints the 3 players with the fastest time
     """
     # first reorders the google sheet by time
-    SHEET.worksheet('scores').sort((3, 'asc'))
+    SHEET.worksheet("scores").sort((3, "asc"))
     # gets values in the google sheet
-    leader_board = SHEET.worksheet('scores').get_all_values()
+    leader_board = SHEET.worksheet("scores").get_all_values()
 
     # Tries to print the leaderboard and if it cant prints the statement:
     # 'No players have played yet'
@@ -53,8 +53,12 @@ def print_top_players():
                 player_n = leader_board[player]
                 print(
                     colored(
-                        "{:20s} {:30s}".format(" " * 5 + player_n[0],
-                                               player_n[1] + "\n"), "blue"))
+                        "{:20s} {:30s}".format(
+                            " " * 5 + player_n[0], player_n[1] + "\n"
+                        ),
+                        "blue",
+                    )
+                )
         else:
             raise IndexError()
     # Otherwise tell the user no players to show
