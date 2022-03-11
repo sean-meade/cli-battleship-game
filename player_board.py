b841839c46e225b5db816bcc5ef6ac78ab3da2c4
Sean <seanpatmeade@gmail.com>
2022-03-11 01:39:13 +0000

    Reformat player_board.py to PEP8 with black.vercel.app

--- player_board.py
+++ player_board.py
@@ -22,6 +22,7 @@ class PlayerBoard:
     symbol = a single character string that fills out the side of the board
     name = a string that's comp by default
     """
+
     # Create player board, set name, symbol and hits
     def __init__(self, symbol, name=COMP):
         self.board = [[symbol] * COLUMNS for _ in range(ROWS)]
@@ -43,27 +44,32 @@ class PlayerBoard:
             # it with a blank space (" ") when printing
             for x in range(len(self.board)):
                 for y in range(len(self.board)):
-                    if self.board[x][y] == '#':
-                        whole_board[x].append(' ')
+                    if self.board[x][y] == "#":
+                        whole_board[x].append(" ")
                     else:
                         whole_board[x].append(self.board[x][y])
         else:
             whole_board = self.board
-
         # print the board to the screen
         for line_num, line in enumerate(whole_board):
             # Prints the top line of the board
             if line_num == 0:
-                print(f"""{top_of_board}
+                print(
+                    f"""{top_of_board}
 {line_spacer}
      {LETTERS[line_num]}  |  {'  '.join(line)}  |
-{line_spacer}""")
+{line_spacer}"""
+                )
             # Prints the bottom line of the board
             elif line_num == 9:
-                print(f"""     {LETTERS[line_num]}  |  {'  '.join(line)}  |
+                print(
+                    f"""     {LETTERS[line_num]}  |  {'  '.join(line)}  |
 {bottom_of_board}
-\t   {'  '.join(NUMS)}""")
+\t   {'  '.join(NUMS)}"""
+                )
             # Prints the lines between
             else:
-                print(f"""     {LETTERS[line_num]}  |  {'  '.join(line)}  |
-{line_spacer}""")
+                print(
+                    f"""     {LETTERS[line_num]}  |  {'  '.join(line)}  |
+{line_spacer}"""
+                )
